72e5cdb48b61dcec8c2c74191565be9dd79ba88f
alex <al3xback@gmail.com>
2024-06-10 12:09:36 +0700

    fixed minor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import { html } from 'htm/preact';
 import styles from './Footer.module.css';
 
 export default function Footer() {
-	return html`<footer className="${styles.footer}">
-		<div className="${styles.container}">
+	return html`<footer className=${styles.footer}>
+		<div className=${styles.container}>
 			<p>
 				Challenge by${' '}
 				<a
